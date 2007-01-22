5e8feee1f29b24b65a4be91dddceb1d89890dc7b
Robert 'phaylon' Sedlacek <rs@474.at>
2007-01-22 16:15:33 +0000

    Import of PHAYLON/Filter-EOF-0.01 from CPAN.
    gitpan-cpan-distribution: Filter-EOF
    gitpan-cpan-version:      0.01
    gitpan-cpan-path:         PHAYLON/Filter-EOF-0.01.tar.gz
    gitpan-cpan-author:       PHAYLON
    gitpan-cpan-maturity:     released

---
+++ t/lib/SimpleFilter.pm
@@ -0,0 +1,33 @@
+package SimpleFilter;
+use warnings;
+use strict;
+
+use Filter::EOF;
+use base 'Exporter';
+
+our @EXPORT = qw(test_export);
+
+sub import {
+    my ($class, @args) = @_;
+    Filter::EOF->on_eof_call(sub { 
+
+        $SimpleTest::TEST_ON_EOF       = 1;
+        $SimpleTest::TEST_ON_EOF_CLASS = $class;
+        $SimpleTest::TEST_PHASE = 'run';
+
+        push @{ $SimpleTest::TEST_ORDER }, 'on_eof';
+    });
+
+    $SimpleTest::TEST_ON_IMPORT       = 1;
+    $SimpleTest::TEST_ON_IMPORT_CLASS = $class;
+    $SimpleTest::TEST_ON_IMPORT_ARGS  = \@args;
+    $SimpleTest::TEST_PHASE = 'compile';
+
+    $SimpleTest::TEST_ORDER = ['on_import'];
+
+    $class->export_to_level(1);
+}
+
+sub test_export { 23 }
+
+1;
